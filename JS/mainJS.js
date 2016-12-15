45e5c78695714c585c74ea339355ff57233a2d1a
William Meldrum <meldrumr1@hotmail.com>
2016-12-14 19:06:40 -0700

    _

--- JS/mainJS.js
+++ JS/mainJS.js
@@ -1,5 +1,5 @@
 console.log("I'm working!");
-$(".play-button").addClass("animated buttonSpin");
+
 
 
 
--- JS/mediaDisplay.js
+++ JS/mediaDisplay.js
@@ -1,17 +1,33 @@
-/* on click(
-
-if does not have active class
-
-function() {
-addClass("animated buttonSpin active");
-append video/media to article post
-slidedownanimation perhaps?
-
-if has active class
-reverse buttonSpin 
-remove active class
-slideup media and pop out of dom
+//console.log("mediaDisplay.js is connected");
 
 
+var $mediaLink = $(".media-link");
+var $mediaContainer = $(".mediaContainer");
+var $video = $("<iframe></iframe>",
+{align: "bottom",src:"https://www.youtube.com/embed/gHLtiVfYYDI"});
 
+   $mediaLink.click(function(event) {
+  event.preventDefault();
+  if ($mediaLink.hasClass("active")) {
+    $(this).removeClass("active");
+    console.log("active class removed");
+  }
+  else {
+  $(this).addClass("active");
+  $(".media-container").append($video).addClass("iframe-video-container");
+  console.log("media has been clicked!");
+  }
 });
+
+if ($mediaLink.hasClass("active")) {
+  console.log("media is active");
+
+}
+else {
+  /*if has active class
+  reverse buttonSpin
+  remove active class
+  slideup media and pop out of dom
+
+  */
+}
