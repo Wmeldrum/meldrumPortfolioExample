50b47d8bf78c16be8dfe8591d43844be86f7849e
William Meldrum <meldrumr1@hotmail.com>
2017-01-01 19:48:00 -0700

    _

--- JS/app.js
+++ JS/app.js
@@ -21,14 +21,18 @@
               title: "NLBC Tournament Results: Battles Fought, Sticks Thrown",
               date: "2016-12-02T20:01:39+00:00",
               desc: "We've all had our moments. Renowned east coast player, AD Sanford Kelly (Sagat) faced off against Mentos sponsored Rico Suave (Abel)... ",
-              author: "William Meldrum"
+              author: "William Meldrum",
+              thumbnail: 'img/Thumbnails/sandfordVsRico.png',
+              isMoment: true
             },
 
             {
               title: 'Capcom Cup 2016 Results: Watch the Riveting Grand Finals to see who walks away with $230k',
               date: '2016-12-04T20:01:39+00:00',
               desc: 'Capcom Cup 2016 Results: Watch the Riveting Grand Finals to see who walks away with $230k',
-              author: 'William Meldrum'
+              author: 'William Meldrum',
+              thumbnail: 'img/Thumbnails/Capcom-Cup-2016.jpg',
+              isMoment: false
            }
 
 
